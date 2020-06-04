2ff597fa2898f283fafedfc709107ed6f48cc1c6
Jorge Sánchez Blanco <jorgesanchezblanco1989@gmail.com>
2020-06-04 06:34:55 +0200

    Completed search by keywords route

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,10 @@ const Twitter = require("twitter-lite");
 
 router.post("/get-user-tweets", async (req, res, next) => {
   try {
-    
-    
-    
-    const {screen_name}= req.body
-    console.log(req.body)
-    //console.log(screen_name)
-
-
+    let { screen_name } = req.body;
+    if (screen_name == undefined) {
+      screen_name = "jack";
+    }
 
     const user = new Twitter({
       consumer_key: "FTY5JrHbwRrKzcJ4vJveLHtXZ",
@@ -27,11 +23,39 @@ router.post("/get-user-tweets", async (req, res, next) => {
       count: 15,
     });
     delete tweets["_headers"];
-    console.log("Length: " + tweets.length);
-    
+
     res.set("Access-Control-Allow-Origin", "*");
     res.status(200).json(tweets);
+  } catch (error) {
+    next(error);
+  }
+});
 
+router.post("/get-tweets-by-keyword", async (req, res, next) => {
+  try {
+    let { q, lang } = req.body.data;
+    if (q == undefined) {
+      q = "Angular";
+    }
+
+    const user = new Twitter({
+      consumer_key: "FTY5JrHbwRrKzcJ4vJveLHtXZ",
+      consumer_secret: "ARrYPjueBIHCIGEW1GI2MkEs7O34P1kfxQ7hSKg0OZSuoqziK5",
+    });
+    const response = await user.getBearerToken();
+    const app = new Twitter({
+      bearer_token: response.access_token,
+    });
+
+    const tweets = await app.get("search/tweets", {
+      q,
+      lang,
+      count: 15,
+    });
+    delete tweets["_headers"];
+
+    res.set("Access-Control-Allow-Origin", "*");
+    res.status(200).json(tweets);
   } catch (error) {
     next(error);
   }
